90659f67b28f20f896e4f62c569d7eed75dd6444
KDFcode <aleksandarristic93@gmail.com>
2023-03-07 10:43:39 +0100

    dodat kontroler

---
+++ MacevalackaAplikacija/Controllers/gbyyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacevalackaAplikacija.Controllers
+{
+    public class gbyyController : Controller
+    {
+        // GET: gbyy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
